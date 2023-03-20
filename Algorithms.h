caa6b0ad524cf8f78ef31c4ec35052d4c9c45c1b
unknown <joerefaat22@gmail.com>
2023-03-20 21:32:24 +0200

    new commit

--- Algorithms.h
+++ Algorithms.h
@@ -1,5 +1,4 @@
-
-<<<<<<< HEAD
+#pragma once
 #include <iostream>
 using namespace std;
 
@@ -8,32 +7,6 @@ namespace sortlib
     template <class T>
     void InsertionSort(T arr[], int size)
     {
-=======
-#ifndef SORTING_ALOGORITMS_PACKAGE_ALGORITHMS_H
-#define SORTING_ALOGORITMS_PACKAGE_ALGORITHMS_H
-
-namespace sortLib {
-
-    template <class T>
-    void printArr(T arr[], int n){
-        for (int i = 0; i < n - 1; ++i) {
-            std::cout << arr[i] << ", ";
-        }
-        std::cout << arr[n-1] << std::endl;
-    }
-
-    template <class T>
-    void insertionSort(T data[], int n){
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; ++j) {
-                if(data[j] > data[i]){
-                    T temp = data[i];
-                    data[i] = data[j];
-                    data[j] = temp;
-                }
-            }
-        }
->>>>>>> 24872d0647832d042f95e500771e61209c36a020
     }
     template <class T>
     void selectionSort(T arr[], int size)
