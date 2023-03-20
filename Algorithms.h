24872d0647832d042f95e500771e61209c36a020
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-03-20 20:54:42 +0200

    adding Insertion Sort

--- Algorithms.h
+++ Algorithms.h
@@ -7,8 +7,25 @@
 
 namespace sortLib {
 
-    void InsertionSort() {
+    template <class T>
+    void printArr(T arr[], int n){
+        for (int i = 0; i < n - 1; ++i) {
+            std::cout << arr[i] << ", ";
+        }
+        std::cout << arr[n-1] << std::endl;
+    }
 
+    template <class T>
+    void insertionSort(T data[], int n){
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; ++j) {
+                if(data[j] > data[i]){
+                    T temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
     }
 
     void selectionSort() {
--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include "Algorithms.h"
 
 int main() {
-    sortLib::InsertionSort();
+    int arr[] = {2,5,8,7,4};
+    int n = 5;
 
+    sortLib::printArr(arr, n);
+
+    sortLib::insertionSort(arr, n);
+
+    sortLib::printArr(arr, n);
     return 0;
 }
