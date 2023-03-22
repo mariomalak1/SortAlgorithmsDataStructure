8464298d99826445fb6eeac11d7e09bd4be45dc6
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-03-22 18:50:36 +0200

    handle main menu and adding bubble sort for now

--- Algorithms.h
+++ Algorithms.h
@@ -185,5 +185,15 @@ namespace sortLib
             delete[] count;
             delete[] output_arr;
         }
+
+        template<class t>
+        void static bubbleSort(t data[], int n) {
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = n - 1; j > i; --j) {
+                    if (data[j] < data[j - 1])
+                        swap(data[j], data[j - 1]);
+                }
+            }
+        }
     };
 }
