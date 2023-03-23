43fcfe05d412a7f259261c1ad7ffc2dff549b326
unknown <joerefaat22@gmail.com>
2023-03-23 15:06:30 +0200

    FIXING main and final edit

--- Algorithms.h
+++ Algorithms.h
@@ -3,197 +3,240 @@ using namespace std;
 
 namespace sortLib
 {
-    class Algorithms{
+    template <class T>
+    void merge(T arr[], int l, int m, int r)
+    {
+        // m = l + ( r - l ) / 2
 
-    private:
-        template<class T>
-        void static merge(T arr[], int l, int m, int r) {
-            // m = l + ( r - l ) / 2
+        int i, j, k = 0;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int *L = new int[n1];
+        int *R = new int[n2];
 
-            int i, j, k = 0;
-            int n1 = m - l + 1;
-            int n2 = r - m;
-            int *L = new int[n1];
-            int *R = new int[n2];
+        // fill the new left array
+        for (i = 0; i < n1; ++i)
+        {
+            L[i] = arr[l + i];
+        }
 
-            // fill the new left array
-            for (i = 0; i < n1; ++i) {
-                L[i] = arr[l + i];
-            }
+        // fill the new right array
+        for (j = 0; j < n2; ++j)
+        {
+            R[j] = arr[j + m + l];
+        }
 
-            // fill the new right array
-            for (j = 0; j < n2; ++j) {
-                R[j] = arr[j + m + l];
-            }
+        i = j = 0;
+        k = l;
 
-            i = j = 0;
-            k = l;
-
-            while (i < n1 && j < n2) {
-                // get smaller first
-                if (L[i] >= R[j]) {
-                    arr[k] = R[j];
-                    j++;
-                } else {
-                    arr[k] = L[i];
-                    i++;
-                }
-                k++;
-            }
-
-            // put the remaining part in the arr1 in the arr3
-            while (i < n1) {
-                arr[k] = L[i];
-                i++;
-                k++;
-            }
-
-            while (j < n2) {
+        while (i < n1 && j < n2)
+        {
+            // get smaller first
+            if (L[i] >= R[j])
+            {
                 arr[k] = R[j];
                 j++;
-                k++;
             }
-        }
-
-
-    public:
-        template<class T>
-        void static printArr(T arr[], int n) {
-            for (int i = 0; i < n - 1; ++i) {
-                std::cout << arr[i] << ", ";
-            }
-            std::cout << arr[n - 1] << std::endl;
-        }
-
-        template<class T>
-        void static insertionSort(T data[], int n) {
-            for (int i = 1; i < n; i++) {
-                for (int j = 0; j < i; ++j) {
-                    if (data[j] > data[i]) {
-                        T temp = data[i];
-                        data[i] = data[j];
-                        data[j] = temp;
-                    }
-                }
-            }
-        }
-
-        template<class T>
-        void static selectionSort(T arr[], int size) {
-            int min;
-            for (int i = 0; i < size - 1; i++) {
-                min = i;
-                for (int j = i + 1; j < size; j++) {
-                    if (arr[j] < arr[min]) {
-                        min = j;
-                    }
-                }
-                swap(arr[min], arr[i]);
-            }
-        }
-
-        void static shellSort(int arr[], int n) {
-            for (int gap = n / 2; gap > 0; gap /= 2) {
-                // divide data into sub arrays
-                for (int i = gap; i < n; i++) {
-                    int tmp = arr[i];
-
-                    int j = i;
-
-                    // sort sub array
-                    for (; j >= gap && arr[j - gap] > tmp; j -= gap) {
-                        arr[j] = arr[j - gap];
-                    }
-
-                    arr[j] = tmp;
-                }
-            }
-        }
-
-
-        template<class T>
-        void static mergeSort(T arr[], int left, int right) {
-            if (left < right) {
-                int m = left + (right - left) / 2;
-                mergeSort(arr, left, m);
-                mergeSort(arr, m + 1, right);
-                merge(arr, left, m, right);
-            }
-        }
-
-        template<class T>
-        void static quickSort(T arr[], int left, int right) {
-            int i = left, j = right;
-            T tmp;
-            T pivot = arr[(left + right) / 2];
-
-            // Partition the array
-            while (i <= j) {
-                while (arr[i] < pivot) {
-                    i++;
-                }
-                while (arr[j] > pivot) {
-                    j--;
-                }
-                if (i <= j) {
-                    tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-                    i++;
-                    j--;
-                }
-            };
-
-            // Recursion
-            if (left < j) {
-                quickSort(arr, left, j);
-            }
-            if (i < right) {
-                quickSort(arr, i, right);
-            }
-        }
-
-
-        template<class T>
-        void static countSort(T arr[], int size) {
-            T *output_arr = new T[size + 1];
-            int maxEl = arr[0];
-            for (int i = 1; i < size; i++) // getting max element in the array
+            else
             {
-                if (arr[i] > maxEl) {
-                    maxEl = arr[i];
-                }
+                arr[k] = L[i];
+                i++;
             }
-            long long int *count = new long long int[maxEl + 1];
-            // initializing count array to = 0
-            for (int i = 0; i <= maxEl; i++) {
-                count[i] = 0;
-            }
-            for (int i = 0; i < size; i++) {
-                count[arr[i]]++;
-            }
-            for (int i = 1; i <= maxEl; i++) {
-                count[i] += count[i - 1];
-            }
-            for (int i = 0; i < size; i++) {
-                output_arr[count[arr[i]] - 1] = arr[i];
-                count[arr[i]]--;
-            }
-            for (int i = 0; i < size; i++) {
-                arr[i] = output_arr[i];
-            }
-            delete[] count;
-            delete[] output_arr;
+            k++;
         }
 
-        template<class t>
-        void static bubbleSort(t data[], int n) {
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = n - 1; j > i; --j) {
-                    if (data[j] < data[j - 1])
-                        swap(data[j], data[j - 1]);
+        // put the remaining part in the arr1 in the arr3
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    template <class T>
+    void printArr(T arr[], int n)
+    {
+        for (int i = 0; i < n - 1; ++i)
+        {
+            std::cout << arr[i] << ", ";
+        }
+        std::cout << arr[n - 1] << std::endl;
+    }
+
+    template <class T>
+    void insertionSort(T data[], int n)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (data[j] > data[i])
+                {
+                    T temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
                 }
             }
         }
-    };
+    }
+
+    template <class T>
+    void selectionSort(T arr[], int size)
+    {
+        int min;
+        for (int i = 0; i < size - 1; i++)
+        {
+            min = i;
+            for (int j = i + 1; j < size; j++)
+            {
+                if (arr[j] < arr[min])
+                {
+                    min = j;
+                }
+            }
+            swap(arr[min], arr[i]);
+        }
+    }
+    template <class T>
+    void shellSort(T arr[], int n)
+    {
+        for (int gap = n / 2; gap > 0; gap /= 2)
+        {
+            // divide data into sub arrays
+            for (int i = gap; i < n; i++)
+            {
+                int tmp = arr[i];
+
+                int j = i;
+
+                // sort sub array
+                for (; j >= gap && arr[j - gap] > tmp; j -= gap)
+                {
+                    arr[j] = arr[j - gap];
+                }
+
+                arr[j] = tmp;
+            }
+        }
+    }
+
+    template <class T>
+    void mergeSort(T arr[], int left, int right)
+    {
+        if (left < right)
+        {
+            int m = left + (right - left) / 2;
+            mergeSort(arr, left, m);
+            mergeSort(arr, m + 1, right);
+            merge(arr, left, m, right);
+        }
+    }
+
+    template <class T>
+    void quickSort(T arr[], int left, int right)
+    {
+        int i = left, j = right;
+        T tmp;
+        T pivot = arr[(left + right) / 2];
+
+        // Partition the array
+        while (i <= j)
+        {
+            while (arr[i] < pivot)
+            {
+                i++;
+            }
+            while (arr[j] > pivot)
+            {
+                j--;
+            }
+            if (i <= j)
+            {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                i++;
+                j--;
+            }
+        };
+
+        // Recursion
+        if (left < j)
+        {
+            quickSort(arr, left, j);
+        }
+        if (i < right)
+        {
+            quickSort(arr, i, right);
+        }
+    }
+
+    template <class T>
+    void countSort(T arr[], int size)
+    {
+        T *output_arr = new T[size + 1];
+
+        int maxEl = arr[0];
+
+        for (int i = 1; i < size; i++) // getting max element in the array
+        {
+            if (arr[i] > maxEl)
+            {
+                maxEl = arr[i];
+            }
+        }
+
+        long long int *count = new long long int[maxEl + 1];
+
+        // initializing count array to = 0
+
+        for (int i = 0; i <= maxEl; i++)
+        {
+            count[i] = 0;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            count[arr[i]]++;
+        }
+
+        for (int i = 1; i <= maxEl; i++)
+        {
+            count[i] += count[i - 1];
+        }
+        for (int i = 0; i < size; i++)
+        {
+
+            output_arr[count[arr[i]] - 1] = arr[i];
+            count[arr[i]]--;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = output_arr[i];
+        }
+        delete[] count;
+        delete[] output_arr;
+    }
+
+    template <class t>
+    void static bubbleSort(t data[], int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = n - 1; j > i; --j)
+            {
+                if (data[j] < data[j - 1])
+                    swap(data[j], data[j - 1]);
+            }
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -3,182 +3,232 @@
 #include <conio.h>
 #include <chrono>
 #include <cstdlib>
-
+using namespace sortLib;
 using namespace std;
 
-class MainClass {
-private:
-    void static chooseAlgorithmsDisplayMenu(){
-        cout << "=============================================" << endl;
-        cout << "       Select One Algorithm to Try." << endl;
-        cout << "=============================================" << endl;
-        cout << "1- Insertion sort" << endl;
-        cout << "2-selection sort" << endl;
-        cout << "3-bubble sort" << endl;
-        cout << "4-shell sort" << endl;
-        cout << "5-count sort" << endl;
-        cout << "6-merge sort" << endl;
-        cout << "7-quick sort" << endl;
-        cout << "8 - Quit" << endl;
-        cout << "Your Choice --> : ";
+int arrMenu()
+{
+    int choice;
+    int size;
+    system("cls");
+start:
+    cout << "What is the size of the array you want to try:" << endl;
+    cout << "1-200" << endl;
+    cout << "2-500" << endl;
+    cout << "3-1000" << endl;
+    cout << "4-5000" << endl;
+    cout << "5-10000" << endl;
+    cout << "6-20000" << endl;
+    cout << "7-50000" << endl;
+    cout << "Your Choice --> :" << endl;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        size = 200;
+        break;
+    }
+    case 2:
+    {
+        size = 500;
+        break;
+    }
+    case 3:
+    {
+        size = 1000;
+        break;
+    }
+    case 4:
+    {
+        size = 5000;
+        break;
+    }
+    case 5:
+    {
+        size = 10000;
+        break;
+    }
+    case 6:
+    {
+        size = 20000;
+        break;
+    }
+    case 7:
+    {
+        size = 50000;
+        break;
+    }
+    default:
+    {
+        system("cls");
+        cout << "##       Wrong INPUT        ##" << endl;
+        goto start;
+    }
+    }
+    return size;
+}
+void TestingAlgo(int choice)
+{
+    string algoName;
+    int size;
+    size = arrMenu();
+    int *arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = (rand() % 10000);
     }
 
-    int static getAlgorithmNumber() {
-        chooseAlgorithmsDisplayMenu();
-        string choose;
-        cin >> choose;
-        if (choose == "1") {
-            return 1;
-        } else if (choose == "2") {
-            return 2;
-        } else if (choose == "3") {
-            return 3;
-        } else if (choose == "4") {
-            return 4;
-        } else if (choose == "5") {
-            return 5;
-        } else if (choose == "6") {
-            return 6;
-        } else if (choose == "7") {
-            return 7;
-        } else if (choose == "8"){
-            return 8;
-        }
-        else {
-            system("cls");
-            cout << "=============================================" << endl;
-            cerr << "##     Wrong Input.. Please Try Again.     ##" << endl;
-            getAlgorithmNumber();
-        }
-        return 0;
+    auto start = chrono::high_resolution_clock::now();
+    switch (choice)
+    {
+    case 1:
+    {
+        algoName = "insertion Sort";
+        insertionSort(arr, size);
+        break;
+    }
+    case 2:
+    {
+        algoName = "Selection Sort";
+        selectionSort(arr, size);
+        break;
+    }
+    case 3:
+    {
+        algoName = "Bubble Sort";
+        bubbleSort(arr, size);
+        break;
+    }
+    case 4:
+    {
+        algoName = "Shell Sort";
+        shellSort(arr, size);
+        break;
+    }
+    case 5:
+    {
+        algoName = "Merge Sort";
+        mergeSort(arr, 0, size);
+        break;
+    }
+    case 6:
+    {
+        algoName = "Quick Sort";
+        quickSort(arr, 0, size);
+        break;
+    }
+    case 7:
+    {
+        algoName = "Count Sort";
+
+        countSort(arr, size);
+        break;
+    }
+    default:
+    {
+        break;
+    }
     }
 
-    void static getSizeDisplayMenu() {
-        cout << "What is the size of the array you want to try:" << endl;
-        cout << "1-200" << endl;
-        cout << "2-500" << endl;
-        cout << "3-1000" << endl;
-        cout << "4-5000" << endl;
-        cout << "5-10000" << endl;
-        cout << "6-20000" << endl;
-        cout << "7-50000" << endl;
-        cout << "Your Choice --> : ";
-    }
+    auto end = chrono::high_resolution_clock::now();
+    double runTime = double(chrono::duration_cast<chrono::milliseconds>(end - start).count());
+    system("cls");
+    cout << "#############################################" << endl;
+    cout << "\033[31m" << algoName << " RunTime:" << runTime << " milliseconds\033[0m " << endl;
+    cout << "#############################################" << endl;
+    // for (int i = 0; i < size; ++i)
+    // {
+    //     std::cout << arr[i] << ", ";
+    // }
+}
+void displayMenu()
+{
 
-    int static getSize() {
-        getSizeDisplayMenu();
-        string choose;
-        cin >> choose;
-        if (choose == "1") {
-            return 200;
-        } else if (choose == "2") {
-            return 500;
-        } else if (choose == "3") {
-            return 1000;
-        } else if (choose == "4") {
-            return 5000;
-        } else if (choose == "5") {
-            return 10000;
-        } else if (choose == "6") {
-            return 20000;
-        } else if (choose == "7") {
-            return 50000;
-        } else {
-            system("cls");
-            cout << "=============================================" << endl;
-            cerr << "##     Wrong Input.. Please Try Again.     ##" << endl;
-            getSize();
-        }
-        return 0;
-    }
+    int choice;
 
-    void static TestingAlgo(int choice){
-        string algoName;
-        int size;
-        size = getSize();
-        int *arr = new int[size];
+    cout << "Welcome To Our Sorting Algorithms Package." << endl;
+start:
+    cout << "=============================================" << endl;
+    cout << "       Select One Algorithm to Try." << endl;
+    cout << "=============================================" << endl;
+    cout << "1-Insertion Sort." << endl;
+    cout << "2-Selection Sort." << endl;
+    cout << "3-Bubble Sort." << endl;
+    cout << "4-Shell Sort." << endl;
+    cout << "5-Merge Sort." << endl;
+    cout << "6-Quick Sort." << endl;
+    cout << "7-Count Sort." << endl;
+    cout << "8-Exit" << endl;
+    cout << "Which one you want to try--->:" << endl;
+    cin >> choice;
 
-        for (int i = 0; i < size; i++)
-        {
-            arr[i] = 1 + (rand() % 5000);
-        }
+    if (choice > 0 && choice <= 8)
+    {
 
-        auto start = chrono::high_resolution_clock::now();
         switch (choice)
         {
-            case 1:
-            {
-                algoName = "insertion Sort";
-                sortLib::Algorithms::insertionSort(arr, size);
-                break;
-            }
-            case 2:
-            {
-                algoName = "Selection Sort";
-                sortLib::Algorithms::selectionSort(arr, size);
-                break;
-            }
-            case 3:
-            {
-                algoName = "Bubble Sort";
-                sortLib::Algorithms::bubbleSort(arr, size);
-                break;
-            }
-            case 4:
-            {
-                algoName = "Shell Sort";
-                sortLib::Algorithms::shellSort(arr, size);
-                break;
-            }
-            case 5:
-            {
-                algoName = "Merge Sort";
-                sortLib::Algorithms::mergeSort(arr, 0, size);
-                break;
-            }
-            case 6:
-            {
-                algoName = "Quick Sort";
-                sortLib::Algorithms::quickSort(arr, 0, size);
-                break;
-            }
-            case 7:
-            {
-                algoName = "Count Sort";
-
-                sortLib::Algorithms::countSort(arr, size);
-                break;
-            }
-            default:
-            {
-                break;
-            }
+        case 1:
+        {
+            TestingAlgo(1);
+            break;
+        }
+        case 2:
+        {
+            TestingAlgo(2);
+            break;
+        }
+        case 3:
+        {
+            TestingAlgo(3);
+            break;
+        }
+        case 4:
+        {
+            TestingAlgo(4);
+            break;
+        }
+        case 5:
+        {
+            TestingAlgo(5);
+            break;
+        }
+        case 6:
+        {
+            TestingAlgo(6);
+            break;
+        }
+        case 7:
+        {
+            TestingAlgo(7);
+            break;
+        }
+        case 8:
+        {
+            exit(0);
+        }
+        case 0:
+        {
+            goto start;
+            break;
         }
 
-        auto end = chrono::high_resolution_clock::now();
-        double runTime = double(chrono::duration_cast<chrono::milliseconds>(end - start).count());
+        default:
+        {
+            break;
+        }
+        }
+    }
+    else
+    {
         system("cls");
-        cout << "#############################################" << endl;
-        cerr << " # " << algoName << " RunTime:" << runTime << " milliseconds" << endl;
-        cout << "#############################################" << endl;
+        cout << "=============================================" << endl;
+        cout << "##     Wrong Input.. Please Try Again.     ##" << endl;
     }
-
-public:
-    void static displayMenu(){
-        cout << "Welcome To Our Sorting Algorithms Package." << endl;
-
-        while (true) {
-            int choice = getAlgorithmNumber();
-            if (choice > 0 && choice <= 8) {
-                if (choice == 8){
-                    break;
-                }
-                TestingAlgo(choice);
-            }
-        }
-    }
-};
-
-int main(){
-    MainClass::displayMenu();
+    goto start;
+}
+int main()
+{
+    displayMenu();
 }
